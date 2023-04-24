fcf00037793488436cbd81887eaa521bb847f3bf
Alex Nagy <rebeloperalpar@gmail.com>
2023-04-24 11:18:11 +0300

    Initial Commit

---
+++ kindaSwiftUIDemo-Requester/kindaSwiftUIDemo_RequesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  kindaSwiftUIDemo_RequesterApp.swift
+//  kindaSwiftUIDemo-Requester
+//
+//  Created by Alex Nagy on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct kindaSwiftUIDemo_RequesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
